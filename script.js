08a8e874cc590a269c21ab26048ba797b4853b69
Carey Tobore <careytobore@gmail.com>
2023-08-01 22:15:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+
+hamburger.addEventListener('click', (event) =>{
+    body.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(e => e.addEventListener('click',()=>{
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+    overlay.classList.remove('active');
+    
+}));
